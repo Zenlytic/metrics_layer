3ec85fbf609e00bc3ee7626f6c97e07ff8527f64
Paul Blankley <31221512+pblankley@users.noreply.github.com>
2022-04-24 16:54:28 -0600

    Feature/sst versioning (#32)
    * in process work (need to fix dev environment, upgrade poetry)
    
    * remove importlib dependency
    
    Co-authored-by: Paul Blankley <paul@zenlytic.com>

--- metrics_layer/__init__.py
+++ metrics_layer/__init__.py
@@ -1,9 +1,4 @@
 from metrics_layer.cli import cli_group  # noqa
 from metrics_layer.core import MetricsLayerConnection  # noqa
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
-__version__ = importlib_metadata.version(__name__)
+__version__ = "0.6.9"
