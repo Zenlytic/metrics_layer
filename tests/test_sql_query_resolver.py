912e95a1c02089456eeabe7dd90457e77599561e
Cole French <16979554+ColeFrench@users.noreply.github.com>
2024-11-14 17:57:59 -0500

    Add query mark

--- tests/test_sql_query_resolver.py
+++ tests/test_sql_query_resolver.py
@@ -1,7 +1,10 @@
+import pytest
+
 from metrics_layer.core.model.project import Project
 from metrics_layer.core.sql.resolve import SQLQueryResolver
 
 
+@pytest.mark.query
 def test_empty_query(models, views, connections):
     SQLQueryResolver(
         metrics=[],
