5c5e0b585bc3e8beba39d7164fb14a2cade77466
Paul Blankley <paul@zenlytic.com>
2025-05-28 08:44:19 -0600

    add raw timeframe by default

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,6 +4,16 @@ from metrics_layer.core.exceptions import AccessDeniedOrDoesNotExistException
 
 
 @pytest.mark.project
+def test_adding_raw_timeframe_to_dimension_group(connection):
+
+    field = connection.project.get_field_by_name("discounts.order")
+    assert "raw" in field.timeframes
+
+    field = connection.project.get_field("discounts.order_raw")
+    assert field.id() == "discounts.order_raw"
+
+
+@pytest.mark.project
 def test_object_id_functions(connection):
     """
     Tests the object id functions for all possible metrics layer objects.
